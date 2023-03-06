74d625187615b6bdd9a8d65f04cbf9863450effb
ReubenMay <95076512+ReubenMay@users.noreply.github.com>
2023-03-06 04:12:52 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 function preload(){
+    
 }
 function setup(){
-    canvas =createCanvas(640,480)
-    canvas.position(110,250);
-    video = createCapture(VIDEO);
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
-    tint_color="";
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet is initialized")
 }
 function draw(){
-    image(video,0,0,640,480);
-    tint(tint_color);
+    image(video,0,0,300,300);
 }
-function take_snapshot() {
-    save('reuben.png')
+function take_snapshot(){
+    save("reubenrex.png");
 }
-function filter_tint() {
-    tint_color=document.getElementById("colour_name").value
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("nose x ="+results[0].pose.nose.x);
+        console.log("nose y ="+results[0].pose.nose.y);
 
+    }
 }
+
